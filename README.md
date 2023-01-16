82a95c95f1676b1fde4ddbc522cdf1f82d0ce20f
afaisalsahar <afaisalsahar@gmail.com>
2023-01-16 09:46:24 -0500

    TechDoc/ project initial file & directory setup

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# FreeCodeCamp Technical Documentation
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'darkblue': '#0f172a'
+      }
+    },
+    fontFamily: {
+      'itner': ['Inter', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
